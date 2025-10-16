fef58b537094068f643abc9973da04b72a0cfe0a
Ayyappan-M-P <mpayyappan2004@gmail.com>
2025-10-16 22:28:17 +0530

    Math

--- C# tutorial/tempApp/Practice1.cs
+++ C# tutorial/tempApp/Practice1.cs
@@ -160,6 +160,19 @@ public class Practice1
         Convert.ToString
         Convert.ToBoolean
         */
+
+        //Math
+        Console.WriteLine(Math.Abs(-40));
+        Console.WriteLine(Math.Ceiling(4.2));
+        Console.WriteLine(Math.Floor(4.9));
+        Console.WriteLine(Math.Max(4, 9));
+        Console.WriteLine(Math.Min(4, 9));
+        Console.WriteLine(Math.Pow(3, 4));
+        Console.WriteLine(Math.Round(4.6));
+        Console.WriteLine(Math.Sqrt(64));
+        Console.WriteLine(Math.PI);
+        Console.WriteLine(Math.E);
+        
         
         
     }
