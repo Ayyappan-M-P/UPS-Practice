2ee0009b1d485b0f08ed70c553073ca9a4dbe8ef
Ayyappan-M-P <mpayyappan2004@gmail.com>
2025-10-26 19:24:15 +0530

    multidimension and exception

--- C# tutorial/tempApp/Methodandreturn.cs
+++ C# tutorial/tempApp/Methodandreturn.cs
@@ -1,43 +1,43 @@
- class Methodandreturn
-    {
-    static void Main(string[] args)
-    {
-        // return  = returns data back to the place where a method is invoked
-        double x;
-        double y;
-        double result;
+//  class Methodandreturn
+//     {
+//     static void Main(string[] args)
+//     {
+//         // return  = returns data back to the place where a method is invoked
+//         double x;
+//         double y;
+//         double result;
 
-        Console.WriteLine("Enter in number 1: ");
-        x = Convert.ToDouble(Console.ReadLine());
+//         Console.WriteLine("Enter in number 1: ");
+//         x = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Enter in number 2: ");
-        y = Convert.ToDouble(Console.ReadLine());
+//         Console.WriteLine("Enter in number 2: ");
+//         y = Convert.ToDouble(Console.ReadLine());
 
-        result = Multiply(x, y);
+//         result = Multiply(x, y);
 
-        Console.WriteLine(result);
+//         Console.WriteLine(result);
 
-        double total = CheckOut(3.99, 5.75, 15, 1.00, 10.25);
+//         double total = CheckOut(3.99, 5.75, 15, 1.00, 10.25);
 
-        Console.WriteLine(total);
+//         Console.WriteLine(total);
 
-        Console.ReadKey();
-    }
-    static double Multiply(double x, double y)
-    {
-        return x * y;
-    }
-            //params keyword = a method parameter that takes a variable number of arguments.
-            //                 The parameter type must be a single - dimensional array
+//         Console.ReadKey();
+//     }
+//     static double Multiply(double x, double y)
+//     {
+//         return x * y;
+//     }
+//             //params keyword = a method parameter that takes a variable number of arguments.
+//             //                 The parameter type must be a single - dimensional array
 
-    static double CheckOut(params double[] prices)
-    {
-        double total = 0;
+//     static double CheckOut(params double[] prices)
+//     {
+//         double total = 0;
 
-        foreach (double price in prices)
-        {
-            total += price;
-        }
-        return total;
-    }
-}
+//         foreach (double price in prices)
+//         {
+//             total += price;
+//         }
+//         return total;
+//     }
+// }
--- C# tutorial/tempApp/obj/Debug/net8.0/tempApp.AssemblyInfo.cs
+++ C# tutorial/tempApp/obj/Debug/net8.0/tempApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tempApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459f8fd7e96f54a0c593e33de20176a8e2dce634")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e8fc12967a2d92bfd6bba54577d652bf94335f")]
 [assembly: System.Reflection.AssemblyProductAttribute("tempApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tempApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
