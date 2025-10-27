eec40873bc9fbc24154047187ada97967a2c5a28
Ayyappan-M-P <mpayyappan2004@gmail.com>
2025-10-27 11:21:47 +0530

    interface

--- C# tutorial/tempApp/Methodoverride.cs
+++ C# tutorial/tempApp/Methodoverride.cs
@@ -1,35 +1,35 @@
-class Program
-{
-    static void Main(string[] args) {
+// class Program
+// {
+//     static void Main(string[] args) {
 
-        //method overriding = provides a new version of a method inherited from a parent class
-            //                    inherited method must be: abstract, virtual, or already overriden
-            //                    Used with ToString(), polymorphism 
+//         //method overriding = provides a new version of a method inherited from a parent class
+//             //                    inherited method must be: abstract, virtual, or already overriden
+//             //                    Used with ToString(), polymorphism 
 
-        Dog dog = new Dog();
-        Cat cat = new Cat();
+//         Dog dog = new Dog();
+//         Cat cat = new Cat();
 
-        dog.Speak();
-        cat.Speak();
+//         dog.Speak();
+//         cat.Speak();
 
-        Console.ReadKey();
-    }
-}
-class Animal 
-{
-    public virtual void Speak()
-    {
-        Console.WriteLine("The animal goes *brrr*");
-    }
-}
-class Dog : Animal
-{
-    public override void Speak()
-    {
-        Console.WriteLine("The dog goes *woof*");
-    }
-}
-class Cat : Animal
-{
+//         Console.ReadKey();
+//     }
+// }
+// class Animal 
+// {
+//     public virtual void Speak()
+//     {
+//         Console.WriteLine("The animal goes *brrr*");
+//     }
+// }
+// class Dog : Animal
+// {
+//     public override void Speak()
+//     {
+//         Console.WriteLine("The dog goes *woof*");
+//     }
+// }
+// class Cat : Animal
+// {
 
-}
+// }
