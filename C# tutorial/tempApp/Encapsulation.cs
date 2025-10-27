e2f669fc4b841630f0c0d338826eecfc4499d468
Ayyappan-M-P <mpayyappan2004@gmail.com>
2025-10-27 15:02:08 +0530

    generic,enum,threading

--- C# tutorial/tempApp/Encapsulation.cs
+++ C# tutorial/tempApp/Encapsulation.cs
@@ -1,47 +1,48 @@
-class Program
-{
-    static void Main(string[] args)
-    {
-            //getters & setters = add security to fields by encapsulation
-            //                    They're accessors found within properties
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//             //getters & setters = add security to fields by encapsulation
+//             //                    They're accessors found within properties
 
-            // properties = combine aspects of both fields and methods (share name with a field)
-            // get accessor = used to return the property value
-            // set accessor = used to assign a new value
-            // value keyword = defines the value being assigned by the set (parameter)
+//             // properties = combine aspects of both fields and methods (share name with a field)
+//             // get accessor = used to return the property value
+//             // set accessor = used to assign a new value
+//             // value keyword = defines the value being assigned by the set (parameter)
             
-        Car car = new Car(400);
+//         Car car = new Car(400);
 
-        car.Speed = 1000000000;
+//         car.Speed = 1000000000;
 
-        Console.WriteLine(car.Speed);
+//         Console.WriteLine(car.Speed);
 
-        Console.ReadKey();
-    }   
-}
-class Car
-{
-    private int speed;
+//         Console.ReadKey();
+//     }   
+// }
+// class Car
+// {
+//     private int speed;
+//     //public string Speed{ get; set; } // auto-implemented property
 
-    public Car(int speed)
-    {
-        Speed = speed;
-    }
+//     public Car(int speed)
+//     {
+//         Speed = speed;
+//     }
 
-    public int Speed
-    {
-        get { return speed; }
-        set                   
-        {
-            if (value > 500)
-            {
-                speed = 500;
-            }
-            else
-            {
-                speed = value;
-            }
-        }
-    }
+//     public int Speed
+//     {
+//         get { return speed; }
+//         set                   
+//         {
+//             if (value > 500)
+//             {
+//                 speed = 500;
+//             }
+//             else
+//             {
+//                 speed = value;
+//             }
+//         }
+//     }
 
-}
+// }
