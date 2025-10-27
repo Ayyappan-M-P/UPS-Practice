f9b9099346ac1cf662726066c284011990bff29f
Ayyappan-M-P <mpayyappan2004@gmail.com>
2025-10-27 10:45:19 +0530

    override

--- C# tutorial/tempApp/obj/Debug/net8.0/tempApp.AssemblyInfo.cs
+++ C# tutorial/tempApp/obj/Debug/net8.0/tempApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tempApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee0009b1d485b0f08ed70c553073ca9a4dbe8ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e104a66fc3fc946eb5a16e8e706faf32afe03692")]
 [assembly: System.Reflection.AssemblyProductAttribute("tempApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tempApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
