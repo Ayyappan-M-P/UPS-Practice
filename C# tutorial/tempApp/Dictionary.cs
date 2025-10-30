a2099643f53e470fc9b710805c5e6f158656bb4e
Ayyappan-M-P <mpayyappan2004@gmail.com>
2025-10-30 10:08:32 +0530

    task

--- C# tutorial/tempApp/Dictionary.cs
+++ C# tutorial/tempApp/Dictionary.cs
@@ -1,38 +1,38 @@
-using System;
-using System.Collections.Generic;
+// using System;
+// using System.Collections.Generic;
 
-class Dictionary
-{
-    static void Main()
-    {
-        // Create a dictionary with <string, int>
-        Dictionary<string, int> studentMarks = new Dictionary<string, int>();
+// class Dictionary
+// {
+//     static void Main()
+//     {
+//         // Create a dictionary with <string, int>
+//         Dictionary<string, int> studentMarks = new Dictionary<string, int>();
 
-        // Adding key-value pairs
-        studentMarks.Add("Ayyappan", 95);
-        studentMarks.Add("Kumar", 88);
-        studentMarks.Add("Rahul", 76);
+//         // Adding key-value pairs
+//         studentMarks.Add("Ayyappan", 95);
+//         studentMarks.Add("Kumar", 88);
+//         studentMarks.Add("Rahul", 76);
 
-        // Accessing value using key
-        Console.WriteLine("Ayyappan's Marks: " + studentMarks["Ayyappan"]);
+//         // Accessing value using key
+//         Console.WriteLine("Ayyappan's Marks: " + studentMarks["Ayyappan"]);
 
-        // Updating value
-        studentMarks["Kumar"] = 90;
+//         // Updating value
+//         studentMarks["Kumar"] = 90;
 
-        // Checking if a key exists
-        if (studentMarks.ContainsKey("Rahul"))
-        {
-            Console.WriteLine("Rahul is in the list with marks: " + studentMarks["Rahul"]);
-        }
+//         // Checking if a key exists
+//         if (studentMarks.ContainsKey("Rahul"))
+//         {
+//             Console.WriteLine("Rahul is in the list with marks: " + studentMarks["Rahul"]);
+//         }
 
-        // Iterating through dictionary
-        Console.WriteLine("\nAll Students and Marks:");
-        foreach (KeyValuePair<string, int> kvp in studentMarks)
-        {
-            Console.WriteLine($"Name: {kvp.Key}, Marks: {kvp.Value}");
-        }
+//         // Iterating through dictionary
+//         Console.WriteLine("\nAll Students and Marks:");
+//         foreach (KeyValuePair<string, int> kvp in studentMarks)
+//         {
+//             Console.WriteLine($"Name: {kvp.Key}, Marks: {kvp.Value}");
+//         }
 
-        // Removing a key
-        studentMarks.Remove("Rahul");
-    }
-}
+//         // Removing a key
+//         studentMarks.Remove("Rahul");
+//     }
+// }
---
+++ C# tutorial/tempApp/Program.cs
@@ -0,0 +1,73 @@
+using System;
+//1
+class A{
+    public static int count = 0;
+    static A() {
+        count++;
+        Console.WriteLine("Static");
+    }
+    public A()
+    {
+        count++;
+        Console.WriteLine("Instance");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Start");
+        A a1 = new A();
+        A a2 = new A();
+        Console.WriteLine($"Count = {A.count}");
+    }
+}
+
+//2
+
+//class Program
+// {
+//     static void Compute(int x, ref int y, out int z)
+//     {
+//         z = x + y;
+//         y = z * 2;
+//         System.Console.WriteLine($"x:{x},y:{y},z:{z}");
+//     }
+//     static void Main()
+//     {
+//         int a = 3, b = 4, c;
+//         Compute(a, ref b, out c);
+//         System.Console.WriteLine($"a:{a},b:{b},c:{c}");
+//     }
+// }
+
+
+//3
+// class Base
+// {
+//     protected int x;
+//     public Base(int a)
+//     {
+//         x = a;
+//         Console.WriteLine($"Base a={x}");
+//     }
+
+// }
+// class Derived : Base
+// {
+
+//     public Derived(int a, int b) : base(b)
+//     {
+//         int sum = a+b;
+//         Console.WriteLine($"Derived sum={sum}");
+//     }
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Derived d = new Derived(2, 3);
+//     }
+// }
