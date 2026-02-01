4f0c5a767601ca3fd7eb56e332b18d12729ed02f
Ayyappan-M-P <mpayyappan2004@gmail.com>
2026-02-01 23:08:23 +0530

    extention methods

---
+++ C# tutorial/practice/Extension.cs
@@ -0,0 +1,56 @@
+public class Circle
+{
+    public double Radius{ get; set; }
+    public Circle(double radius)
+    {
+        this.Radius=radius;
+    }
+}
+public class Rectangle
+{
+    public double Length{ get; set; }
+    public double Width{ get; set; }
+    public Rectangle(double length, double width)
+    {
+        this.Length=length;
+        this.Width=width;
+    }
+}
+public class Triangle
+{
+    public double Base{ get; set; }
+    public double Height{ get; set; }
+    public Triangle(double b, double height)
+    {
+        this.Base=b;
+        this.Height=height;
+    }
+}
+
+public static class ShapeExtensions
+{
+    public static double GetArea(this Circle c)
+    {
+        return Math.PI*c.Radius*c.Radius;
+    }
+    public static double GetArea(this Rectangle r)
+    {
+        return r.Length*r.Width;
+    }
+    public static double GetArea(this Triangle t)
+    {
+        return 0.5*t.Base*t.Height;
+    }
+}
+class Extension
+{
+    public static void Main(string[] args)
+    {
+        Circle c=new Circle(5);
+        Rectangle r=new Rectangle(4,6);
+        Triangle t=new Triangle(3,4);
+        Console.WriteLine("Area of Circle: "+c.GetArea());
+        Console.WriteLine("Area of Rectangle: "+r.GetArea());
+        Console.WriteLine("Area of Triangle: "+t.GetArea());
+    }
+}
